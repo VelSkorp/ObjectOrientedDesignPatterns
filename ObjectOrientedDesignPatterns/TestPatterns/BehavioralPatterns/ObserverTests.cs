52c088ab2b9d15dc266c98908b5ebeb8217b7201
VelSkorp <ukantsevich@gmail.com>
2024-09-05 22:31:43 +0200

    Added test case for Observer testing

---
+++ ObjectOrientedDesignPatterns/TestPatterns/BehavioralPatterns/ObserverTests.cs
@@ -0,0 +1,21 @@
+using BehavioralPatterns.Observer;
+
+namespace TestPatterns
+{
+	[TestFixture]
+	public class ObserverTests
+	{
+		[Test]
+		public void ObserverTest()
+		{
+			var editor = new Editor();
+			var logger = new LoggingListener("/path/to/log.txt", "Someone has opened the file: %s");
+			editor.Events.Subscribe("open", logger);
+
+			var stream = editor.OpenFile("/path/to/file.txt");
+			stream.Close();
+
+			Assert.That(File.Exists("/path/to/log.txt"), Is.True);
+		}
+	}
+}
