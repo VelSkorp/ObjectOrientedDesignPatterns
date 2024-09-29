421037840775ba6800cd29d60eff679300bb5e47
VelSkorp <ukantsevich@gmail.com>
2024-09-29 21:58:02 +0200

    Added Code Smells and Refactoring Techniques

---
+++ ObjectOrientedDesignPatterns/RefactoringTechniques/ComposingMethods/ExtractMethod/Problem.cs
@@ -0,0 +1,14 @@
+﻿namespace RefactoringTechniques.ComposingMethods.ExtractMethod
+{
+	public class Problem
+	{
+		public void PrintOwing()
+		{
+			PrintBanner();
+
+			// Print details.
+			Console.WriteLine("name: " + name);
+			Console.WriteLine("amount: " + GetOutstanding());
+		}
+	}
+}
---
+++ ObjectOrientedDesignPatterns/RefactoringTechniques/ComposingMethods/ExtractMethod/Solution.cs
@@ -0,0 +1,17 @@
+﻿namespace RefactoringTechniques.ComposingMethods.ExtractMethod
+{
+	public class Solution
+	{
+		public void PrintOwing()
+		{
+			PrintBanner();
+			PrintDetails();
+		}
+
+		public void PrintDetails()
+		{
+			Console.WriteLine("name: " + name);
+			Console.WriteLine("amount: " + GetOutstanding());
+		}
+	}
+}
