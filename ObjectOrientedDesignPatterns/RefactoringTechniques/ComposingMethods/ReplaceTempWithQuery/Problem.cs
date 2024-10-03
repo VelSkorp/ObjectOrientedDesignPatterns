5c3de6239e546bd1e798a6e15b8a7aa47d1269d7
VelSkorp <ukantsevich@gmail.com>
2024-10-03 18:01:07 +0200

    Added Data Clumps bloater; Added replace temp with query technique

---
+++ ObjectOrientedDesignPatterns/RefactoringTechniques/ComposingMethods/ReplaceTempWithQuery/Problem.cs
@@ -0,0 +1,22 @@
+﻿namespace RefactoringTechniques.ComposingMethods.ReplaceTempWithQuery
+{
+	public class Problem
+	{
+		private double _quantity = 10;
+		private double _itemPrice = 10;
+
+		public double CalculateTotal()
+		{
+			double basePrice = _quantity * _itemPrice;
+
+			if (basePrice > 1000)
+			{
+				return basePrice * 0.95;
+			}
+			else
+			{
+				return basePrice * 0.98;
+			}
+		}
+	}
+}
---
+++ ObjectOrientedDesignPatterns/RefactoringTechniques/ComposingMethods/ReplaceTempWithQuery/Solution.cs
@@ -0,0 +1,25 @@
+﻿namespace RefactoringTechniques.ComposingMethods.ReplaceTempWithQuery
+{
+	public class Solution
+	{
+		private double _quantity = 10;
+		private double _itemPrice = 10;
+
+		public double CalculateTotal()
+		{
+			if (BasePrice() > 1000)
+			{
+				return BasePrice() * 0.95;
+			}
+			else
+			{
+				return BasePrice() * 0.98;
+			}
+		}
+
+		public double BasePrice()
+		{
+			return _quantity * _itemPrice;
+		}
+	}
+}
