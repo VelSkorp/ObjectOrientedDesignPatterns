612b5b167c45416e380d41cf8c75f901a9edc7d2
VelSkorp <ukantsevich@gmail.com>
2024-10-04 18:56:30 +0200

    Added Object-Orientation Abusers code smells; Added Switch Statements code smells; Added Split Temporary Variable technique

---
+++ ObjectOrientedDesignPatterns/RefactoringTechniques/ComposingMethods/SplitTemporaryVariable/Problem.cs
@@ -0,0 +1,16 @@
+﻿namespace RefactoringTechniques.ComposingMethods.SplitTemporaryVariable
+{
+	public class Problem
+	{
+		private double _height = 10;
+		private double _width = 10;
+
+		public void Method()
+		{
+			var temp = 2 * (_height + _width);
+			Console.WriteLine(temp);
+			temp = _height * _width;
+			Console.WriteLine(temp);
+		}
+	}
+}
---
+++ ObjectOrientedDesignPatterns/RefactoringTechniques/ComposingMethods/SplitTemporaryVariable/Solution.cs
@@ -0,0 +1,16 @@
+﻿namespace RefactoringTechniques.ComposingMethods.SplitTemporaryVariable
+{
+	public class Solution
+	{
+		private double _height = 10;
+		private double _width = 10;
+
+		public void Method()
+		{
+			var perimeter = 2 * (_height + _width);
+			Console.WriteLine(perimeter);
+			var area = _height * _width;
+			Console.WriteLine(area);
+		}
+	}
+}
