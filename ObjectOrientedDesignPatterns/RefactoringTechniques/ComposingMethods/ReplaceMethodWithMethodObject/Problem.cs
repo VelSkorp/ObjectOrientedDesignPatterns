a0f673bcc6cceeb7deecc1181e9533854a9addf4
VelSkorp <ukantsevich@gmail.com>
2024-10-05 22:51:17 +0200

    Added Alternative Classes With Different Interfaces code smell; Added Replace Method With Method Object technique

---
+++ ObjectOrientedDesignPatterns/RefactoringTechniques/ComposingMethods/ReplaceMethodWithMethodObject/Problem.cs
@@ -0,0 +1,17 @@
+﻿namespace RefactoringTechniques.ComposingMethods.ReplaceMethodWithMethodObject
+{
+	public class Problem
+	{
+		public class Order
+		{
+			public double Price()
+			{
+				double primaryBasePrice = 5.5;
+				double secondaryBasePrice = 10.9;
+				double tertiaryBasePrice = 30;
+				// Perform long computation.
+				return primaryBasePrice + secondaryBasePrice + tertiaryBasePrice;
+			}
+		}
+	}
+}
---
+++ ObjectOrientedDesignPatterns/RefactoringTechniques/ComposingMethods/ReplaceMethodWithMethodObject/Solution.cs
@@ -0,0 +1,35 @@
+﻿namespace RefactoringTechniques.ComposingMethods.ReplaceMethodWithMethodObject
+{
+	public class Solution
+	{
+		public class Order
+		{
+			public double BasePrice => 5.5;
+
+			public double Price()
+			{
+				return new PriceCalculator(this).Compute();
+			}
+		}
+
+		public class PriceCalculator
+		{
+			private readonly double primaryBasePrice;
+			private readonly double secondaryBasePrice = 10.9;
+			private readonly double tertiaryBasePrice = 30;
+
+			public PriceCalculator(Order order)
+			{
+				// Copy relevant information from the
+				// order object.
+				primaryBasePrice = order.BasePrice;
+			}
+
+			public double Compute()
+			{
+				// Perform long computation.
+				return primaryBasePrice + secondaryBasePrice + tertiaryBasePrice;
+			}
+		}
+	}
+}
